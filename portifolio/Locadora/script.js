327b81ae30c9e4ee742fdd13688a7131527cb1f2
CarlosNatan0101 <106560487+CarlosNatan0101@users.noreply.github.com>
2023-02-20 20:41:10 -0300

    primeiro commit

---
+++ portifolio/Locadora/script.js
@@ -0,0 +1,15 @@
+function validarForm () {
+    let validaCod = document.forms ["cadastrar"] ["codigo"] .value;
+
+    if (validaCod == ""){
+        alert ("O código está deveras incorreto");
+        return false;
+    }
+}
+
+function insere () {
+    let codi = document.forms [cadastrar][codigo] .value
+    let titu = document.forms [cadastrar][titulo] .value
+    let dire = document.forms [cadastrar][diretor] .value
+    let 
+}
